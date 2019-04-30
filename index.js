c0daa628e0be6a658aba0e603483fe9954781852
Erin Green <erin.y.green@gmail.com>
2019-04-30 15:52:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const currentUser = "Erin";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, `
